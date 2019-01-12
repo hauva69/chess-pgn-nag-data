46f552bdd2c04dff7d8f902e51692aa576eac20b
Ari Makela <ari.makela@iki.fi>
2019-01-13 00:08:02 +0200

    Parse lines into NAG structs

--- standard2json.go
+++ standard2json.go
@@ -5,7 +5,20 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 )
+
+// NAG (Numeric Annotation Glyphs or NAGs are used to annotate chess games),
+// providing an assessment of a chess move or a chess position.
+type NAG struct {
+	Value       int    `json:"value"`
+	Description string `json:"description"`
+}
+
+func (n NAG) String() string {
+	return fmt.Sprintf("%d\t%s", n.Value, n.Description)
+}
 
 func main() {
 	const firstNAGLine = 1398
@@ -14,7 +27,7 @@ func main() {
 	filename := "doc/pgnstd.txt"
 	file, err := os.Open(filename)
 	if err != nil {
-		log.Fatalf("Unable to open file %q: %s", filename, err)
+		log.Fatalf("Unable to open file %q: %q", filename, err)
 	}
 
 	defer file.Close()
@@ -28,10 +41,18 @@ func main() {
 			log.Fatalf("Unable to read %q: %s", line, err)
 		}
 
-		if lineCount > lastNAGLine {
+		if lineCount >= lastNAGLine {
 			break
 		} else if lineCount >= firstNAGLine {
-			fmt.Println(line)
+			fields := strings.Fields(line)
+			value, err := strconv.Atoi(fields[0])
+			if err != nil {
+				log.Fatalf("Unable to parse NAG: %s", err)
+			}
+			description := strings.Join(fields[1:], " ")
+			nag := NAG{value, description}
+			fmt.Println(nag)
+
 		}
 
 		lineCount++
